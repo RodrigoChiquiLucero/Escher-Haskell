d68938154740dae7895c7faf2c8e477e6acdc463
alexislevi96 <alexislevi96@hotmail.com>
2019-03-29 12:25:54 -0300

    Ejemplos funcionando

--- Basico/Ejemplo.hs
+++ Basico/Ejemplo.hs
@@ -2,15 +2,24 @@ module Basico.Ejemplo where
 import Dibujo
 import Interp
 
-data Bas = T1 | T2
+data Bas = T1 | T2 | TD | F | R
 ejemplo :: Dibujo Bas
 --ejemplo = r180 $ pureDibe ()
-ejemplo = r180 $ pureDibe T1
---ejemplo = encimar4 $ pureDibe ()
---ejemplo = figSobreOtra $ pureDibe () pureDibe
+--ejemplo = r270 $ pureDibe T1
+--ejemplo = figSobreOtra (pureDibe T1) (pureDibe T2)
+--ejemplo = encimar4 (pureDibe T1)
+--ejemplo = figAlLado (pureDibe F) (pureDibe TD)
+--ejemplo = superponeDosFig (pureDibe F) (pureDibe TD)
+--ejemplo = cuarteto (pureDibe F) (pureDibe TD) (pureDibe F) (pureDibe TD)
+--ejemplo = ciclar (pureDibe F)
+
 
 interpBas :: Output Bas
 interpBas T1 = trian1 
+interpBas T2 = trian2
+interpBas TD = trianD
+interpBas F = fShape
+interpBas R = rectan
 
 {-
 interpBas () = juntar 3 3 fShape trian1
