dd6d7389a0169bd55fb326a2bf08620d2e79ec91
marcos <marcoscerioni98@gmail.com>
2019-04-05 12:46:29 -0300

    ESCHER LISTOOOOO

--- Basico/Ejemplo.hs
+++ Basico/Ejemplo.hs
@@ -1,3 +1,4 @@
+
 module Basico.Ejemplo where
 import Dibujo
 import Interp
@@ -15,12 +16,15 @@ ejemplo :: Dibujo Bas
 
 --Escher--
 --Este es el MedioT
+a :: Bas
+a = T2
+
 t :: Dibujo Bas
-t = (encimar4 (pureDibe T2))
+t = (encimar4 (pureDibe a))
 
 --Este es el 3
 u :: Dibujo Bas
-u = figSobreOtra (superponeDosFig (pureDibe T2) (Rot45 (pureDibe T2))) (r270(superponeDosFig (pureDibe T2) (Rot45 (pureDibe T2))))
+u = figSobreOtra (superponeDosFig (pureDibe a) (superponeDosFig (Rot45 (pureDibe a)) (r180 (pureDibe a)))) (r270(superponeDosFig (pureDibe a) (superponeDosFig (Rot45 (pureDibe a)) (r180 (pureDibe a)))))
 
 --Este es el el T chiquito abajo y el 3 grande arriba
 c :: Dibujo Bas
@@ -54,15 +58,28 @@ medioV :: Dibujo Bas
 medioV = Juntar 3 7 u (Apilar 1 1 u u)
 
 medioW :: Dibujo Bas
-medioW = r270 (medioV)
+medioW = (r270 (medioV))
 
 medioS :: Dibujo Bas
-medioS = r180(medioV)
+medioS = (r180 (medioV))
 
 medioQ :: Dibujo Bas
 medioQ = r90 (medioV)
 
+medioSTV :: Dibujo Bas
+medioSTV = Juntar 6 2 medioS (Juntar 3 5 t medioV)
 
+infVWX :: Dibujo Bas
+infVWX = Juntar 6 2 esqInfV (Juntar 3 5 medioW esqInfX)
+
+supPQR :: Dibujo Bas
+supPQR = Juntar 6 2 esqSupP (Juntar 3 5 medioQ esqSupR)
+
+escher :: Dibujo Bas
+escher = Apilar 6 2 supPQR (Apilar 3 5 medioSTV infVWX)
+
+ejemplo = escher
+--superponeDosFig (pureDibe T2) (superponeDosFig (Rot45 (pureDibe T2)) (r180 (pureDibe T2)))
 
 interpBas :: Output Bas
 interpBas T1 = trian1 
